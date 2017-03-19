8baf5825f3af387ca972fae200e25b304797491f
Li Du <lidu@ldu.svl.corp.google.com>
2017-03-19 13:06:07 -0700

    baobao's improvement

--- barn1.cc
+++ barn1.cc
@@ -17,37 +17,32 @@ int main() {
   int num_boards, num_stalls, num_cows;
   fin >> num_boards >> num_stalls >> num_cows;
   std::vector<int> occupied_stalls;
-  // The number of empty stalls between occupied stalls.
-  std::vector<int> empty_stalls;
-
   for (int i = 0; i < num_cows; ++i) {
     int stall_num;
     fin >> stall_num;
     occupied_stalls.push_back(stall_num);
   }
   std::sort(occupied_stalls.begin(), occupied_stalls.end());
-  int last_stall_num = occupied_stalls[0];
+
+  // The number of empty stalls between occupied stalls.
+  std::vector<int> empty_stalls;
   for (int i = 1; i < num_cows; ++i) {
-    int stall_num = occupied_stalls[i];
-    if (stall_num - last_stall_num > 1) {
-      empty_stalls.push_back(stall_num - last_stall_num - 1);
+    int empty_stall = occupied_stalls[i] - occupied_stalls[i - 1];
+    if (empty_stall > 1) {
+      empty_stalls.push_back(empty_stall - 1);
     }
-    last_stall_num = stall_num;
   }
 
   int length = occupied_stalls[num_cows - 1] - occupied_stalls[0] + 1;
   std::sort(empty_stalls.begin(), empty_stalls.end(), std::greater<int>());
-  int empty_size = empty_stalls.size();
-  int i = 0;
-  while (num_boards > 1 && i < empty_size) {
-    length -= empty_stalls[i];
-    ++i;
-    num_boards--;
+  int boards_required = 1;
+  for (int empty_stall : empty_stalls) {
+    if (boards_required >= num_boards)
+      break;
+    ++boards_required;
+    length -= empty_stall;
   }
-
   fout << length << endl;
 
   return 0;
 }
-
-
